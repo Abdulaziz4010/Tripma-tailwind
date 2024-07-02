ab3002791e21f24f8794525f7b9b992892ed6923
Abdulaziz4010 <abdulazizs707ja@gamil.com>
2024-07-02 16:06:09 +0500

    main

---
+++ index.js
@@ -0,0 +1,65 @@
+// let a = 5;
+
+// let b = 5;
+
+// let = a * b;
+
+// console.log(a * b);
+
+// 1. uchta sondan eng kattasini chiqaruvchi algaritm
+
+// let number1 = 10;
+
+// let number2 = 6;
+
+// let number3 = 7;
+
+// if (number1 > number2 && number1 > number3) {
+//   console.log("Number1 katta");
+// } else if (number3 > number1 && number3 > number2) {
+//   console.log("Number3 katta");
+// } else if (number2 > number1 && number2 > number3) {
+//   console.log("Number2 katta");
+// }
+
+//  2. foydalanuvchi mablag’ini kiritadi va mehmonhonada olinadigan hona turini ayting 1000 - lux | 500 - yarim lux yoki 500dan 200 gacha bo’lsa odiy hona 200 dan kam bo’lsa - joy yo’q
+
+// let hotel = +prompt("Hotel narxi");
+
+// if (hotel >= 1000) {
+//   console.log("Lux");
+// } else if (hotel <= 1000 && 500 <= hotel) {
+//   console.log("Yarim lux");
+// } else if (hotel <= 500 && 200 <= hotel) {
+//   console.log("Oddiy xona");
+// } else {
+//   console.log("Xona yoq");
+// }
+
+// 3. foydalanucvchi son kiritadi agar son 3 ga bo’linsa fizz 5 ga bo’lisa bass agar 3 ham 5 ga bo’linsa fizz-bazz
+// let a = prompt("Son kiriting");
+// if (a % 15 == 0) {
+//   console.log("fizz-bazz");
+// } else if (a % 5 == 0) {
+//   console.log("bazz");
+// } else if (a % 3 == 0) {
+//   console.log("fizz");
+// } else {
+//   console.log("Karochchi xato qilding bratish");
+// }
+
+// 4. uchta sondan eng kichigini chiqaruvchi algaritm
+
+// let a = 1;
+
+// let b = 4;
+
+// let c = 7;
+
+// if (a < b && a < c) {
+//   console.log("a kichik");
+// } else if (b < a && b < c) {
+//   console.log("b kichik");
+// } else if (c < a && c < b) {
+//   console.log("c kichik");
+// }
